d0c5080859e88a0a080fd4266f2c5d99b0ba4521
moraes <none@none>
2009-11-03 03:51:28 -0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     author = 'Rodrigo Moraes',
     author_email = 'rodrigo.moraes@gmail.com',
     url = 'http://code.google.com/p/tipfy/',
+    download_url = 'http://tipfy.googlecode.com/files/tipfy.latest.zip',
     description = 'The cute little framework for App Engine',
     license = 'BSD',
     version = '0.2',
     packages = ['tipfy'],
     package_dir = {'': 'source'},
     zip_safe = False,
-    install_requires = ['werkzeug >= 0.6dev', 'jinja2', 'mako', 'babel'],
+    install_requires = ['werkzeug >= 0.6dev'],
+    extras_require = {
+        'Jinja2': ['jinja2'],
+        'Mako':   ['mako'],
+        'Babel':  ['badel'],
+    },
 )

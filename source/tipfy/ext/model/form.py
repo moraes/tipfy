012b37e33fb35edcaf2307b24e0c718e95186f47
moraes <none@none>
2009-11-17 07:17:24 -0200

    Continuing work on ext.model.form.

--- source/tipfy/ext/model/form.py
+++ source/tipfy/ext/model/form.py
@@ -3,39 +3,54 @@
     tipfy.ext.model.forms
     ~~~~~~~~~~~~~~~~~~~~~
 
-    Form generation utilities for db.Model classes.
+    Form generation utilities for db.Model classes, based on wtforms.
 
     Example usage:
 
         from google.appengine.ext import db
         from tipfy.ext.model.form import model_form
 
-        # Define a model.
+        # Define a model and add a record.
         class MyModel(db.Model):
             name = db.StringProperty(default='some default value')
-            number = db.IntegerProperty(required=True)
+            age = db.IntegerProperty(required=True)
+
+        new_entity = MyModel(key_name='test', name='Test Name', age=17)
+        new_entity.put()
 
         # Generate a form based on the model.
         MyModelForm = model_form(MyModel)
-
-        # Add a record.
-        new_entity = MyModel(key_name='test', name='Foo Test Name', number=7)
-        new_entity.put()
 
         # Get a form populated with entity data.
         entity = MyModel.get_by_key_name('test')
         form = MyModelForm(obj=entity)
 
+    The form returned by model_form() can be used as a base class for forms
+    mixing non-model fields and/or other model forms. For example:
+
+        # Generate a form based on the model.
+        BaseContactForm = model_form(MyModel)
+
+        # Generate a form based on other model.
+        ExtraContactForm = model_form(MyOtherModel)
+
+        class ContactForm(BaseContactForm):
+            # Add an extra, non-model related field.
+            subscribe_to_news = f.BooleanField()
+
+            # Add the other model form as a subform.
+            extra = f.FormField(ExtraContactForm)
+
     :copyright: 2009 by tipfy.org.
     :license: BSD, see LICENSE.txt for more details.
 """
-from wtforms import widgets
-from wtforms import fields as f
-from wtforms import Form
-from wtforms import validators
+from wtforms import Form, validators, fields as f
 
 
 class StringListPropertyField(f.TextAreaField):
+    """A field for ``db.StringListProperty``. The list items are rendered in a
+    textarea.
+    """
     def process_data(self, value):
         if isinstance(value, list):
             value = '\n'.join(value)
@@ -52,19 +67,24 @@ class StringListPropertyField(f.TextAreaField):
 
 
 def get_TextField(kwargs):
-    # StringProperty limit: 500 bytes.
+    """Returns a ``TextField``, applying the ``db.StringProperty`` length limit
+    of 500 bytes.
+    """
     kwargs['validators'].append(validators.length(max=500))
     return f.TextField(**kwargs)
 
 
 def get_IntegerField(kwargs):
-    # IntegerProperty limit.
+    """Returns an ``IntegerField``, applying the ``db.IntegerProperty`` range
+    limits.
+    """
     kwargs['validators'].append(validators.NumberRange(min=0x8000000000000000,
         max=0x7fffffffffffffff))
     return f.IntegerField(**kwargs)
 
 
 def convert_StringProperty(model, prop, kwargs):
+    """Returns a form field for a StringProperty."""
     if prop.multiline:
         kwargs['validators'].append(validators.length(max=500))
         return f.TextAreaField(**kwargs)
@@ -73,103 +93,185 @@ def convert_StringProperty(model, prop, kwargs):
 
 
 def convert_ByteStringProperty(model, prop, kwargs):
-    raise NotImplemented()
+    """Returns a form field for a ByteStringProperty."""
+    return get_TextField(kwargs)
 
 
 def convert_BooleanProperty(model, prop, kwargs):
+    """Returns a form field for a BooleanProperty."""
     return f.BooleanField(**kwargs)
 
 
 def convert_IntegerProperty(model, prop, kwargs):
+    """Returns a form field for a IntegerProperty."""
     return get_IntegerField(kwargs)
 
 
 def convert_FloatProperty(model, prop, kwargs):
+    """Returns a form field for a FloatProperty."""
     return get_TextField(kwargs)
 
 
 def convert_DateTimeProperty(model, prop, kwargs):
+    """Returns a form field for a DateTimeProperty."""
     if prop.auto_now or prop.auto_now_add:
         return None
     return f.DateTimeField(format='%Y-%m-%d %H-%M-%S', **kwargs)
 
 
 def convert_DateProperty(model, prop, kwargs):
+    """Returns a form field for a DateProperty."""
     if prop.auto_now or prop.auto_now_add:
         return None
     return f.DateTimeField(format='%Y-%m-%d', **kwargs)
 
 
 def convert_TimeProperty(model, prop, kwargs):
+    """Returns a form field for a TimeProperty."""
     if prop.auto_now or prop.auto_now_add:
         return None
     return f.DateTimeField(format='%H-%M-%S', **kwargs)
 
 
 def convert_ListProperty(model, prop, kwargs):
+    """Returns a form field for a ListProperty."""
     raise NotImplemented()
 
 
 def convert_StringListProperty(model, prop, kwargs):
+    """Returns a form field for a StringListProperty."""
     return StringListPropertyField(**kwargs)
 
 
 def convert_ReferenceProperty(model, prop, kwargs):
+    """Returns a form field for a ReferenceProperty."""
     raise NotImplemented()
 
 
 def convert_SelfReferenceProperty(model, prop, kwargs):
+    """Returns a form field for a SelfReferenceProperty."""
     raise NotImplemented()
 
 
 def convert_UserProperty(model, prop, kwargs):
+    """Returns a form field for a UserProperty."""
     raise NotImplemented()
 
 
 def convert_BlobProperty(model, prop, kwargs):
+    """Returns a form field for a BlobProperty."""
     return f.FileField(**kwargs)
 
 
 def convert_TextProperty(model, prop, kwargs):
+    """Returns a form field for a TextProperty."""
     return f.TextAreaField(**kwargs)
 
 
 def convert_CategoryProperty(model, prop, kwargs):
-    raise NotImplemented()
+    """Returns a form field for a CategoryProperty."""
+    return get_TextField(kwargs)
 
 
 def convert_LinkProperty(model, prop, kwargs):
+    """Returns a form field for a LinkProperty."""
     kwargs['validators'].append(validators.url())
     return get_TextField(kwargs)
 
 
 def convert_EmailProperty(model, prop, kwargs):
+    """Returns a form field for a EmailProperty."""
     kwargs['validators'].append(validators.email())
     return get_TextField(kwargs)
 
 
 def convert_GeoPtProperty(model, prop, kwargs):
+    """Returns a form field for a GeoPtProperty."""
     raise NotImplemented()
 
 
 def convert_IMProperty(model, prop, kwargs):
+    """Returns a form field for a IMProperty."""
     raise NotImplemented()
 
 
 def convert_PhoneNumberProperty(model, prop, kwargs):
+    """Returns a form field for a PhoneNumberProperty."""
     return get_TextField(kwargs)
 
 
 def convert_PostalAddressProperty(model, prop, kwargs):
+    """Returns a form field for a PostalAddressProperty."""
     return get_TextField(kwargs)
 
 
 def convert_RatingProperty(model, prop, kwargs):
-    raise NotImplemented()
+    """Returns a form field for a RatingProperty."""
+    kwargs['validators'].append(validators.NumberRange(min=0, max=100))
+    return f.IntegerField(**kwargs)
 
 
 class ModelConverter(object):
-    """Converts properties from a db.Model class to form fields."""
+    """Converts properties from a ``db.Model`` class to form fields.
+
+    Default conversions between properties and fields:
+
+    +====================+===================+==============+==================+
+    | Property subclass  | Field subclass    | datatype     | notes            |
+    +====================+===================+==============+==================+
+    | StringProperty     | TextField         | unicode      | TextArea         |
+    |                    |                   |              | if multiline     |
+    +--------------------+-------------------+--------------+------------------+
+    | ByteStringProperty | TextField         | str          |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | BooleanProperty    | BooleanField      | bool         |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | IntegerProperty    | IntegerField      | int or long  |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | FloatProperty      | TextField         | float        |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | DateTimeProperty   | DateTimeField     | datetime     | skipped if       |
+    |                    |                   |              | auto_now[_add]   |
+    +--------------------+-------------------+--------------+------------------+
+    | DateProperty       | DateTimeField     | date         | skipped if       |
+    |                    |                   |              | auto_now[_add]   |
+    +--------------------+-------------------+--------------+------------------+
+    | TimeProperty       | DateTimeField     | time         | skipped if       |
+    |                    |                   |              | auto_now[_add]   |
+    +--------------------+-------------------+--------------+------------------+
+    | ListProperty       | None              | list         | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | StringListProperty | TextAreaField     | list of str  |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | ReferenceProperty  | None              | db.Model     | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | SelfReferenceP.    | None              | db.Model     | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | UserProperty       | None              | users.User   | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | BlobProperty       | FileField         | str          |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | TextProperty       | TextAreaField     | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | CategoryProperty   | TextField         | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | LinkProperty       | TextField         | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | EmailProperty      | TextField         | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | GeoPtProperty      | None              | db.GeoPt     | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | IMProperty         | None              | db.IM        | always skipped   |
+    +--------------------+-------------------+--------------+------------------+
+    | PhoneNumberProperty| TextField         | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | PostalAddressP.    | TextField         | unicode      |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | RatingProperty     | IntegerField      | int or long  |                  |
+    +--------------------+-------------------+--------------+------------------+
+    | _ReverseReferenceP.| None              | <iterable>   | always skipped   |
+    +====================+===================+==============+==================+
+    """
     default_converters = {
         'StringProperty':        convert_StringProperty,
         'ByteStringProperty':    convert_ByteStringProperty,
@@ -197,9 +299,24 @@ class ModelConverter(object):
     }
 
     def __init__(self, converters=None):
+        """Constructs the converter, setting the converter callables.
+
+        :param converters:
+            A dictionary of converter callables for each property type. The
+            callable must accept the arguments (model, prop, kwargs).
+        """
         self.converters = converters or self.default_converters
 
     def convert(self, model, prop, field_args):
+        """Returns a form field for a single model property.
+
+        :param model:
+            The ``db.Model`` class that contains the property.
+        :param prop:
+            The model property: a ``db.Property`` instance.
+        :param field_args:
+            Optional keyword arguments to construct the field.
+        """
         kwargs = {
             'label': prop.name,
             'default': prop.default_value(),
@@ -221,26 +338,29 @@ class ModelConverter(object):
                 return method(model, prop, kwargs)
 
 
-def model_form(model, base_class=Form, converter=None, only=None, exclude=None,
-    field_args=None):
-    """Creates a wtforms form for a given db.Model class.
+def model_form(model, base_class=Form, only=None, exclude=None, field_args=None,
+    converter=None):
+    """Creates and returns a dynamic ``wtforms.Form`` class for a a given
+    ``db.Model`` class. The form class can be used as it is or serve as a base
+    for extended form classes, which can then mix non-model related fields,
+    subforms with other model forms, among other possibilities.
 
     :param model:
         The ``db.Model`` class to generate a form for.
     :param base_class:
         Base form class to extend from. Must be a ``wtforms.Form`` subclass.
+    :param only:
+        An optional iterable with the property names that should be included in
+        the form. Only these properties will have fields.
+    :param exclude:
+        An optional iterable with the property names that should be excluded
+        from the form. All other properties will have fields.
+    :param field_args:
+        An optional dictionary of field names mapping to a keyword arguments
+        used to construct each field object.
     :param converter:
         A converter to generate the fields based on the model properties. If
-        not set, ``ModelConverter`` will be used.
-    :param only:
-        An optional iterable with the property names to include in the form.
-        Only these properties will have fields.
-    :param exclude:
-        An optional iterable with the property names to not include in the form.
-        All other properties will have fields.
-    :param field_args:
-        An optional dictionary of field names mapping to a dictionary of
-        arguments used to construct the field object.
+        not set, ``ModelConverter`` is used.
     """
     converter = converter or ModelConverter()
     field_args = field_args or {}

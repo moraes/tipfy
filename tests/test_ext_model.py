15c8a886c26e5acf43f1baaf56ff3cee1e947cfc
moraes <none@none>
2009-11-19 15:02:46 -0200

    Added tests for load_entity().

--- tests/test_ext_model.py
+++ tests/test_ext_model.py
@@ -9,10 +9,10 @@ from google.appengine.ext import db
 from gaetestbed import DataStoreTestCase
 
 from tipfy import NotFound
-from tipfy.ext.model import model_from_protobuf, model_to_protobuf, \
+from tipfy.ext.model import get_entity_from_protobuf, get_protobuf_from_entity, \
     populate_entity, get_by_key_name_or_404, get_by_id_or_404, get_or_404, \
-    get_or_insert_with_flag, get_key, PickleProperty, SlugProperty, \
-    EtagProperty, retry_on_timeout
+    get_or_insert_with_flag, get_reference_key, PickleProperty, SlugProperty, \
+    EtagProperty, retry_on_timeout, load_entity
 
 
 class FooModel(db.Model):
@@ -32,18 +32,13 @@ class BarModel(db.Model):
     foo = db.ReferenceProperty(FooModel)
 
 
-class RetryCounter(object):
-    def __init__(self):
-        self.value = 0
-
-
 @retry_on_timeout(retries=3, interval=0.1)
 def test_timeout_1(**kwargs):
     counter = kwargs.get('counter')
 
     # Let it pass only in the last attempt
-    if counter.value < 3:
-        counter.value += 1
+    if counter[0] < 3:
+        counter[0] += 1
         raise db.Timeout()
 
 
@@ -52,8 +47,8 @@ def test_timeout_2(**kwargs):
     counter = kwargs.get('counter')
 
     # Let it pass only in the last attempt
-    if counter.value < 5:
-        counter.value += 1
+    if counter[0] < 5:
+        counter[0] += 1
         raise db.Timeout()
 
     raise ValueError()
@@ -63,7 +58,7 @@ def test_timeout_2(**kwargs):
 def test_timeout_3(**kwargs):
     # Never let it pass.
     counter = kwargs.get('counter')
-    counter.value += 1
+    counter[0] += 1
     raise db.Timeout()
 
 
@@ -72,9 +67,9 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         entity_1 = FooModel(name='foo', age=15, married=False)
         entity_1.put()
 
-        pb_1 = model_to_protobuf(entity_1)
+        pb_1 = get_protobuf_from_entity(entity_1)
 
-        entity_1 = model_from_protobuf(pb_1)
+        entity_1 = get_entity_from_protobuf(pb_1)
         self.assertEqual(isinstance(entity_1, FooModel), True)
         self.assertEqual(entity_1.name, 'foo')
         self.assertEqual(entity_1.age, 15)
@@ -88,23 +83,23 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         entity_3 = FooModel(name='baz', age=45, married=False)
         entity_3.put()
 
-        pbs = model_to_protobuf([entity_1, entity_2, entity_3])
+        pbs = get_protobuf_from_entity([entity_1, entity_2, entity_3])
         self.assertEqual(len(pbs), 3)
         pb_1, pb_2, pb_3 = pbs
 
-        entity_1 = model_from_protobuf(pb_1)
+        entity_1 = get_entity_from_protobuf(pb_1)
         self.assertEqual(isinstance(entity_1, FooModel), True)
         self.assertEqual(entity_1.name, 'foo')
         self.assertEqual(entity_1.age, 15)
         self.assertEqual(entity_1.married, False)
 
-        entity_2 = model_from_protobuf(pb_2)
+        entity_2 = get_entity_from_protobuf(pb_2)
         self.assertEqual(isinstance(entity_2, FooModel), True)
         self.assertEqual(entity_2.name, 'bar')
         self.assertEqual(entity_2.age, 30)
         self.assertEqual(entity_2.married, True)
 
-        entity_3 = model_from_protobuf(pb_3)
+        entity_3 = get_entity_from_protobuf(pb_3)
         self.assertEqual(isinstance(entity_3, FooModel), True)
         self.assertEqual(entity_3.name, 'baz')
         self.assertEqual(entity_3.age, 45)
@@ -182,7 +177,7 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         self.assertEqual(entity.age, 15)
         self.assertEqual(entity.married, False)
 
-    def test_get_key(self):
+    def test_get_reference_key(self):
         entity_1 = FooModel(name='foo', age=15, married=False)
         entity_1.put()
         entity_1_key = str(entity_1.key())
@@ -194,7 +189,7 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         entity_3 = BarModel.get_by_key_name('first_bar')
         # Won't resolve, but we can still get the key value.
         self.assertRaises(db.Error, getattr, entity_3, 'foo')
-        self.assertEqual(str(get_key(entity_3, 'foo')), entity_1_key)
+        self.assertEqual(str(get_reference_key(entity_3, 'foo')), entity_1_key)
 
     def test_pickle_property(self):
         data_1 = {'foo': 'bar'}
@@ -218,7 +213,6 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         entity_2 = FooModel(key_name='bar', name=u'Tião Macalé')
         entity_2.put()
 
-
         entity_1 = FooModel.get_by_key_name('foo')
         entity_2 = FooModel.get_by_key_name('bar')
         self.assertEqual('mary-bjork', entity_1.slug)
@@ -231,24 +225,59 @@ class TestModel(DataStoreTestCase, unittest.TestCase):
         entity_2 = FooModel(key_name='bar', name=u'Tião Macalé')
         entity_2.put()
 
-
         entity_1 = FooModel.get_by_key_name('foo')
         entity_2 = FooModel.get_by_key_name('bar')
         self.assertEqual(hashlib.sha1(entity_1.name.encode('utf8')).hexdigest(), entity_1.etag)
         self.assertEqual(hashlib.sha1(entity_2.name.encode('utf8')).hexdigest(), entity_2.etag)
 
     def test_retry_on_timeout_1(self):
-        counter = RetryCounter()
+        counter = [0]
         test_timeout_1(counter=counter)
-        self.assertEqual(counter.value, 3)
+        self.assertEqual(counter[0], 3)
 
     def test_retry_on_timeout_2(self):
-        counter = RetryCounter()
+        counter = [0]
         self.assertRaises(ValueError, test_timeout_2, counter=counter)
-        self.assertEqual(counter.value, 5)
+        self.assertEqual(counter[0], 5)
 
     def test_retry_on_timeout_3(self):
-        counter = RetryCounter()
+        counter = [0]
         self.assertRaises(db.Timeout, test_timeout_3, counter=counter)
-        self.assertEqual(counter.value, 3)
+        self.assertEqual(counter[0], 3)
+
+    def test_load_entity_1(self):
+        @load_entity(FooModel, 'foo_key', 'foo', 'key')
+        def get(*args, **kwargs):
+            return kwargs['foo']
+
+        foo = FooModel(name='foo')
+        foo.put()
+
+        loaded_foo = get(foo_key=str(foo.key()))
+        self.assertEqual(str(foo.key()), str(loaded_foo.key()))
+        self.assertEqual(None, get(foo_key=None))
+
+    def test_load_entity_2(self):
+        @load_entity(FooModel, 'foo_id', 'foo', 'id')
+        def get(*args, **kwargs):
+            return kwargs['foo']
+
+        foo = FooModel(name='foo')
+        foo.put()
+
+        loaded_foo = get(foo_id=foo.key().id())
+        self.assertEqual(str(foo.key()), str(loaded_foo.key()))
+        self.assertRaises(NotFound, get, foo_id=-1)
+
+    def test_load_entity_3(self):
+        @load_entity(FooModel, 'foo_key_name', 'foo', 'key_name')
+        def get(*args, **kwargs):
+            return kwargs['foo']
+
+        foo = FooModel(key_name='foo', name='foo')
+        foo.put()
+
+        loaded_foo = get(foo_key_name='foo')
+        self.assertEqual(str(foo.key()), str(loaded_foo.key()))
+        self.assertRaises(NotFound, get, foo_key_name='bar')
 

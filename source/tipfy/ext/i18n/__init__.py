10bc90fca129381066511268eb13d778e9337ebe
moraes <none@none>
2009-11-20 13:14:19 -0200

    Initial support for timezones.
    --HG--
    rename : docs/source/api/ext.taskqueue.rst => docs/source/api/tipfy.ext.taskqueue.rst

--- source/tipfy/ext/i18n/__init__.py
+++ source/tipfy/ext/i18n/__init__.py
@@ -11,7 +11,11 @@
     :copyright: 2009 by tipfy.org.
     :license: BSD, see LICENSE.txt for more details.
 """
-import datetime
+import logging
+from datetime import tzinfo, timedelta
+
+from google.appengine.api import urlfetch
+
 from babel.support import Translations, LazyProxy
 from babel.dates import format_date as babel_format_date, \
     format_datetime as babel_format_datetime, format_time as babel_format_time
@@ -23,8 +27,9 @@ local.locale = local.translations = None
 locale = local('locale')
 translations = local('translations')
 
-# Cache loaded translations in the module
+# Cache loaded translations and timezones.
 _translations = {}
+_timezones = {}
 
 
 class I18nMiddleware(object):
@@ -59,6 +64,54 @@ def set_locale(locale):
     local.translations = _translations[locale]
 
 
+def fetch_jsontime_timezone(tz_name):
+    """Fetches timezone info from a json-time service and returns a tzinfo
+    object. By default it uses the service provided by
+    `http://json-time.appspot.com/`_.
+
+    This function derives from http://github.com/caio/bizarrice
+
+    :param tz_name:
+        A valid timezone name according to the Olson database. For example,
+        'America/Chicago'.
+    :return:
+        A ``datetime.tzinfo`` object. If the timezone is not valid, tzinfo for
+        UTC is returned.
+    """
+    url = getattr(local.app.config, 'json_time_service_url',
+        'http://json-time.appspot.com/time.json')
+
+    tz = _tzinfo_utc
+    result = urlfetch.fetch('%s?tz=%s' % (url, timezone))
+
+    if result.status_code != 200:
+        logging.error('Service JsonTime returned unexpected status code: %d'
+                      % result.status_code)
+    else:
+        from django.utils import simplejson
+        try:
+            json = simplejson.loads(result.content)
+        except ValueError:
+            logging.error('Service JsonTime returned non-json content')
+        else:
+            if json['error']:
+                logging.error('Invalid timezone "%s" in config.py. '
+                              'Falling back to UTC.' % timezone)
+            else:
+                # 'datetime' format is 'Fri, 20 Nov 2009 06:43:17 -0600'
+                # Extract the offset ('-0600').
+                offset = json['datetime'].rsplit(' ', 1)[1]
+
+                # Convert the offset to seconds
+                # '-0600' is converted to ((6 * 3600) + (0 * 60)) * -1
+                tz_offset = (int(offset[1:3]) * 3600) + (int(offset[3:]) * 60)
+                if offset[0] == '-':
+                    tz_offset *= -1
+                tz = date.tzinfo
+
+    return tz
+
+
 # Some functions borrowed from Zine: http://zine.pocoo.org/.
 def gettext(string):
     """Translate a given string to the language of the application."""
@@ -83,21 +136,56 @@ def lazy_ngettext(singular, plural, n):
 
 
 def format_date(value=None, format='medium'):
-    time_diff = app.config.time_diff or datetime.timedelta()
-    return babel_format_date(date=value + time_diff, format=format,
-        locale=local.locale)
+    return babel_format_date(date=value, format=format, locale=local.locale)
 
 
 def format_datetime(value=None, format='medium', tzinfo=None):
-    time_diff = app.config.time_diff or datetime.timedelta()
-    return babel_format_datetime(datetime=value + time_diff, format=format,
-        tzinfo=tzinfo, locale=local.locale)
+    return babel_format_datetime(datetime=value, format=format, tzinfo=tzinfo,
+        locale=local.locale)
 
 
 def format_time(value=None, format='medium', tzinfo=None):
-    time_diff = app.config.time_diff or datetime.timedelta()
-    return babel_format_time(time=value + time_diff, format=format,
-        tzinfo=tzinfo, locale=local.locale)
+    return babel_format_time(time=value, format=format, tzinfo=tzinfo,
+        locale=local.locale)
 
 
+class TimezoneOffset(tzinfo):
+    """
+    This class derives from http://pypi.python.org/pypi/python-dateutil
+    """
+    ZERO = timedelta(0)
+
+    def __init__(self, name, offset):
+        self._name = name
+        if offset == 0:
+            self._offset = self.ZERO
+        else:
+            self._offset = timedelta(seconds=offset)
+
+    def utcoffset(self, dt):
+        return self._offset
+
+    def dst(self, dt):
+        return self.ZERO
+
+    def tzname(self, dt):
+        return self._name
+
+    def __eq__(self, other):
+        return (isinstance(other, TimezoneOffset) and
+                self._offset == other._offset)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return "%s('%s', %s)" % (self.__class__.__name__,
+            self._name, self._offset.days * 86400 + self._offset.seconds)
+
+    __reduce__ = object.__reduce__
+
+
+# Default timezone object.
+_tzinfo_utc = TimezoneOffset('UTC', 0)
+# Alias to gettext.
 _ = gettext

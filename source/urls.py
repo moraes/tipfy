27b790ffe8a38badc0256b89ed0e72120c6a9e16
moraes <none@none>
2010-03-18 13:52:10 -0300

    Added a little explanation to urls.py.

--- source/urls.py
+++ source/urls.py
@@ -8,16 +8,20 @@
     :copyright: 2009 by tipfy.org.
     :license: BSD, see LICENSE.txt for more details.
 """
-from tipfy import get_config, import_string, Rule
+import tipfy
 
 def get_rules():
+    """Returns a list of URL rules for the application. The list can be defined
+    entirely here or in separate ``urls.py`` files. Here we show an example of
+    joining all rules from the ``apps_installed`` listed in config.
+    """
     rules = []
 
-    for app_module in get_config('tipfy', 'apps_installed'):
+    for app_module in tipfy.get_config('tipfy', 'apps_installed'):
         try:
             # Load the urls module from the app and extend our rules.
-            app_urls = import_string('%s.urls' % app_module)
-            rules.extend(app_urls.get_rules())
+            app_rules = tipfy.import_string('%s.urls' % app_module)
+            rules.extend(app_rules.get_rules())
         except ImportError:
             pass
 

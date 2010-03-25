199c41939693ff167777599754a1df0220a5e375
moraes <none@none>
2010-03-25 19:46:15 -0300

    Removing unnecessary debugger monkeypatching. Werkzeug implemented most of what was needed to get debugger working.

--- source/tipfy/ext/debugger/patch.py
+++ source/tipfy/ext/debugger/patch.py
@@ -3,7 +3,7 @@
     tipfy.ext.debugger.patch
     ~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Monkey patches for Werkzeug's interactive debugger to work with the
+    Monkeypatch for Werkzeug's interactive debugger to work with the
     development server.
 
     See http://dev.pocoo.org/projects/jinja/ticket/349
@@ -12,9 +12,9 @@
     :license: BSD, see LICENSE.txt for more details.
 """
 import sys
-import inspect
 from os.path import join, dirname
 from jinja2 import Environment, FileSystemLoader
+
 
 env = Environment(loader=FileSystemLoader([join(dirname(__file__),
     'templates')]))
@@ -29,40 +29,5 @@ def render_template(template_filename, **context):
     return get_template(template_filename).render(**context)
 
 
-# werkzeug.debug.console.HTMLStringO
-def flush(self):
-    pass
-
-
-def seek(self, n, mode=0):
-    pass
-
-
-def readline(self):
-    if len(self._buffer) == 0:
-        return ''
-    ret = self._buffer[0]
-    del self._buffer[0]
-    return ret
-
-
-# werkzeug.debug.console.ThreadedStream
-def push():
-    from werkzeug.debug.console import _local
-    if not isinstance(sys.stdout, ThreadedStream):
-        sys.stdout = ThreadedStream()
-    _local.stream = HTMLStringO()
-
-
-# Patch utils first, to avoid loading Werkzeug's template.
+# Patch utils to use Jinja templates instead.
 sys.modules['werkzeug.debug.utils'] = sys.modules[__name__]
-
-# Patch inspect. getsourcefile() is empty on App Engine.
-inspect.getsourcefile = inspect.getfile
-
-# Apply all other patches.
-from werkzeug.debug.console import HTMLStringO, ThreadedStream
-HTMLStringO.flush = flush
-HTMLStringO.seek = seek
-HTMLStringO.readline = readline
-ThreadedStream.push = staticmethod(push)

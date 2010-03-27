752ec119db9c3d3c736444c346613a891f271356
Bryan McLemore <kaelten@gmail.com>
2010-03-26 22:06:17 -0500

    added an ext module to enable appstats

---
+++ source/tipfy/ext/appstats/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    tipfy.ext.appstats
+    ~~~~~~~~~~~~~~~~~~~~
+    
+    Sets up the appstats middleware.  Can be used either in production or development.
+
+"""
+
+def setup(app):
+    """
+    Setup this extension. It wraps the application by AppEngine's appstats middleware.
+
+    To enable it, add this module to the list of extensions in ``config.py``:
+
+    .. code-block:: python
+
+       config = {
+           'tipfy': {
+               'extensions': [
+                   'tipfy.ext.appstats',
+                   # ...
+               ],
+           },
+       }
+
+    :param app:
+        The WSGI application instance.
+    :return:
+        ``None``.
+    """
+
+    from google.appengine.ext.appstats.recording import appstats_wsgi_middleware
+    app.hooks.add('pre_run_app', appstats_wsgi_middleware)

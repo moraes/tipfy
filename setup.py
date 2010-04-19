d4c7a0234dc6a8289431deed2f7198fdc46e149c
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-04-19 01:46:56 -0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,21 +1,57 @@
+"""
+Tipfy
+=====
+Tipfy is a small but powerful framework made specifically for
+`Google App Engine <http://code.google.com/appengine/>`_. It is a lot like
+`webapp <http://code.google.com/appengine/docs/python/tools/webapp/>`_::
+
+    from tipfy import RequestHandler, Response
+
+    class HelloWorldHandler(RequestHandler):
+        def get(self):
+            return Response('Hello, World!')
+
+
+...but has features and goodies that webapp misses: i18n, sessions,
+own authentication, flash messages and more. Everything in a modular,
+lightweight way, tuned for App Engine. You use only what you need, when you
+need.
+
+
+Links
+-----
+* `website <http://www.tipfy.org/>`_
+* `documentation <http://www.tipfy.org//docs/>`_
+"""
 from setuptools import setup
 
 setup(
     name = 'tipfy',
+    version = '0.5.1',
+    license = 'BSD',
+    url = 'http://www.tipfy.org/',
+    download_url = 'http://www.tipfy.org/downloads/tipfy.latest.tar.bz2',
+    description = 'An almighty little framework made specifically for Google App Engine',
+    long_description=__doc__,
     author = 'Rodrigo Moraes',
     author_email = 'rodrigo.moraes@gmail.com',
-    url = 'http://code.google.com/p/tipfy/',
-    download_url = 'http://tipfy.googlecode.com/files/tipfy.latest.zip',
-    description = 'The cute little framework for App Engine',
-    license = 'BSD',
-    version = '0.5rc',
     packages = ['tipfy'],
     package_dir = {'': 'source'},
     zip_safe = False,
-    install_requires = ['werkzeug >= 0.6'],
-    extras_require = {
-        'Jinja2': ['jinja2'],
-        'Mako':   ['mako'],
-        'Babel':  ['badel'],
-    },
+    platforms='any',
+    install_requires = [
+        'werkzeug >= 0.6',
+        'Jinja2>=2.4',
+        'babel',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )

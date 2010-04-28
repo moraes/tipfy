fdba82fdedc913e8ec84ff468947038e9a2c62d6
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-04-28 10:46:26 -0300

    Updating buildout.cfg and organizing buildout dir.

--- buildout/app/main.py
+++ buildout/app/main.py
@@ -8,7 +8,6 @@
     :copyright: 2009 by tipfy.org.
     :license: BSD, see LICENSE for more details.
 """
-import os
 import sys
 
 if 'lib' not in sys.path:
@@ -20,9 +19,11 @@ import tipfy
 # Instantiate the application.
 application = tipfy.make_wsgi_app(config.config)
 
+
 def main():
     # Run it!
     tipfy.run_wsgi_app(application)
+
 
 if __name__ == '__main__':
     main()

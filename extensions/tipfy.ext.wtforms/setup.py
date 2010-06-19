54de79103c392315cf275b649a6e65cd9a01aefd
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-06-19 14:06:27 -0300

    ext.wtforms 0.4

--- extensions/tipfy.ext.wtforms/setup.py
+++ extensions/tipfy.ext.wtforms/setup.py
@@ -26,7 +26,7 @@ from setuptools import setup
 
 setup(
     name = 'tipfy.ext.wtforms',
-    version = '0.3',
+    version = '0.4',
     license = 'BSD',
     url = 'http://www.tipfy.org/',
     description = 'WTForms extension for tipfy',

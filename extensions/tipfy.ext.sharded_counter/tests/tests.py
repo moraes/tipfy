73bd8b55276e5358f920c264df47bb5ccf8380dd
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-07-07 11:59:30 -0300

    Now with tests. :)

--- extensions/tipfy.ext.sharded_counter/tests/tests.py
+++ extensions/tipfy.ext.sharded_counter/tests/tests.py
@@ -32,6 +32,9 @@ class TestCounter(DataStoreTestCase, MemcacheTestCase,
     def test_counter(self):
         # Build a new counter that uses the unique key name 'hits'.
         hits = Counter('hits')
+
+        assert hits.count == 0
+
         # Increment by 1.
         hits.increment()
         # Increment by 10.
@@ -49,3 +52,30 @@ class TestCounter(DataStoreTestCase, MemcacheTestCase,
 
         assert hits.get_count(nocache=True) == 6
 
+    def test_cache(self):
+        # Build a new counter that uses the unique key name 'hits'.
+        hits = Counter('hits')
+
+        assert hits.count == 0
+
+        # Increment by 1.
+        hits.increment()
+        # Increment by 10.
+        hits.increment(10)
+        # Decrement by 3.
+        hits.increment(-3)
+        # This is the current count.
+        assert hits.count == 8
+
+        # Forces fetching a non-cached count of all shards.
+        assert hits.get_count(nocache=True) == 8
+
+        # Set the counter to an arbitrary value.
+        hits.delete()
+
+        assert hits.get_count() == 8
+        assert hits.get_count(nocache=True) == 0
+
+        hits.memcached.delete_count()
+
+        assert hits.get_count() == 0

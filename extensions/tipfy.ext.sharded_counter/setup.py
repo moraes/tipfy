1eb073f39161c958633766fbda03330bc1764012
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-07-07 09:07:12 -0300

    Adding bare port from Bill Katz' sharded counter. Still need to add a configuration option or factory for number of shards used by the counter.

---
+++ extensions/tipfy.ext.sharded_counter/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+tipfy.ext.sharded_counter
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A general purpose sharded counter implementation for App Engine.
+
+Documentation is available at
+`http://www.tipfy.org/wiki/extensions/sharded_counter/ <http://www.tipfy.org/wiki/extensions/sharded_counter/>`_.
+
+Links
+-----
+* `Tipfy <http://www.tipfy.org/>`_
+* `API Documentation <http://www.tipfy.org/docs/>`_
+* `Wiki <http://www.tipfy.org/wiki/>`_
+* `Discussion Group <http://groups.google.com/group/tipfy>`_
+* `Issue Tracker <http://code.google.com/p/tipfy/issues/list>`_
+* `Source Code Repository <http://code.google.com/p/tipfy/>`_
+"""
+from setuptools import setup
+
+setup(
+    name = 'tipfy.ext.sharded_counter',
+    version = '0.1',
+    license = 'Apache',
+    url = 'http://www.tipfy.org/',
+    description = 'A general purpose sharded counter implementation for the App Engine',
+    long_description = __doc__,
+    author = 'Rodrigo Moraes',
+    author_email = 'rodrigo.moraes@gmail.com',
+    zip_safe = False,
+    platforms = 'any',
+    packages = [
+        'tipfy',
+        'tipfy.ext',
+    ],
+    namespace_packages = [
+        'tipfy',
+        'tipfy.ext',
+    ],
+    include_package_data = True,
+    install_requires = [
+        'tipfy',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
---
+++ extensions/tipfy.ext.sharded_counter/tipfy/ext/sharded_counter.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+    tipfy.ext.sharded_counter
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    A general purpose sharded counter implementation for the datastore.
+
+    :copyright: 2008 William T Katz.
+    :copyright: 2010 Rodrigo Moraes.
+    :license: Apache, see LICENSE.txt for more details.
+"""
+import string
+import random
+import logging
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from google.appengine.runtime import apiproxy_errors
+
+
+class MemcachedCount(object):
+    DELTA_ZERO = 500000   # Allows negative numbers in unsigned memcache
+
+    def __init__(self, name):
+        self.key = 'MemcachedCount' + name
+
+    def get_count(self):
+        value = memcache.get(self.key)
+        if value is None:
+            return 0
+        else:
+            return string.atoi(value) - MemcachedCount.DELTA_ZERO
+
+    def set_count(self, value):
+        memcache.set(self.key, str(MemcachedCount.DELTA_ZERO + value))
+
+    def delete_count(self):
+        memcache.delete(self.key)
+
+    count = property(get_count, set_count, delete_count)
+
+    def increment(self, incr=1):
+        value = memcache.get(self.key)
+        if value is None:
+            self.count = incr
+        elif incr > 0:
+            memcache.incr(self.key, incr)
+        elif incr < 0:
+            memcache.decr(self.key, -incr)
+
+
+class Counter(object):
+    """A counter using sharded writes to prevent contentions.
+
+    Should be used for counters that handle a lot of concurrent use.
+    Follows pattern described in Google I/O talk:
+        http://sites.google.com/site/io/building-scalable-web-applications-with-google-app-engine
+
+    Memcache is used for caching counts and if a cached count is available, it is
+    the most correct. If there are datastore put issues, we store the un-put values
+    into a delayed_incr memcache that will be applied as soon as the next shard put
+    is successful. Changes will only be lost if we lose memcache before a successful
+    datastore shard put or there's a failure/error in memcache.
+
+    Usage:
+        hits = Counter('hits')
+        hits.increment()
+        my_hits = hits.count
+        hits.get_count(nocache=True)  # Forces non-cached count of all shards
+        hits.count = 6                # Set the counter to arbitrary value
+        hits.increment(incr=-1)       # Decrement
+        hits.increment(10)
+    """
+    NUM_SHARDS = 20
+
+    def __init__(self, name):
+        self.name = name
+        self.memcached = MemcachedCount('Counter' + name)
+        self.delayed_incr = MemcachedCount('DelayedIncr' + name)
+
+    def delete(self):
+        q = db.Query(CounterShard).filter('name =', self.name)
+        shards = q.fetch(limit=Counter.NUM_SHARDS)
+        db.delete(shards)
+
+    def get_count_and_cache(self):
+        q = db.Query(CounterShard).filter('name =', self.name)
+        shards = q.fetch(limit=Counter.NUM_SHARDS)
+        datastore_count = 0
+        for shard in shards:
+            datastore_count += shard.count
+        count = datastore_count + self.delayed_incr.count
+        self.memcached.count = count
+        return count
+
+    def get_count(self, nocache=False):
+        total = self.memcached.count
+        if nocache or total is None:
+            return self.get_count_and_cache()
+        else:
+            return int(total)
+
+    def set_count(self, value):
+        cur_value = self.get_count()
+        self.memcached.count = value
+        delta = value - cur_value
+        if delta != 0:
+            CounterShard.increment(self, incr=delta)
+
+    count = property(get_count, set_count)
+
+    def increment(self, incr=1, refresh=False):
+        CounterShard.increment(self, incr)
+        self.memcached.increment(incr)
+
+
+class CounterShard(db.Model):
+    name = db.StringProperty(required=True)
+    count = db.IntegerProperty(default=0)
+
+    @classmethod
+    def increment(cls, counter, incr=1):
+        index = random.randint(1, Counter.NUM_SHARDS)
+        counter_name = counter.name
+        delayed_incr = counter.delayed_incr.count
+        shard_key_name = 'Shard' + counter_name + str(index)
+        def get_or_create_shard():
+            shard = CounterShard.get_by_key_name(shard_key_name)
+            if shard is None:
+                shard = CounterShard(key_name=shard_key_name, name=counter_name)
+            shard.count += incr + delayed_incr
+            key = shard.put()
+        try:
+            db.run_in_transaction(get_or_create_shard)
+        except (db.Error, apiproxy_errors.Error), e:
+            counter.delayed_incr.increment(incr)
+            logging.error("CounterShard (%s) delayed increment %d: %s",
+                          counter_name, incr, e)
+            return False
+        if delayed_incr:
+            counter.delayed_incr.count = 0
+        return True

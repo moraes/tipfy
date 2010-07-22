84620480abf8d59496c382376796d72d574198ce
Peter Morgan <pedromorgan@gmail.com>
2010-07-22 21:19:34 +0100

    Initial docs test patch and pedro testing mercurial

--- extensions/tipfy.ext.appstats/tipfy/ext/appstats.py
+++ extensions/tipfy.ext.appstats/tipfy/ext/appstats.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-    tipfy.ext.appstats
-    ~~~~~~~~~~~~~~~~~~
+= tipfy.ext.appstats
 
-    Sets up the appstats middleware. Can be used either in production or
-    development.
-
-    :copyright: 2010 by tipfy.org.
-    :license: BSD, see LICENSE.txt for more details.
+Sets up the appstats middleware. Can be used either in production or
+development.
 """
 from google.appengine.ext.appstats.recording import appstats_wsgi_middleware
 
 
 class AppstatsMiddleware(object):
     def post_make_app(self, app):
-        """Wraps the application by by App Engine's appstats.
-
-        :param app:
-            The WSGI application instance.
-        :return:
-            The application, wrapped or not.
+        """Wraps the application by App Engine's appstats.
+        * params: 
+        ** **app**: The WSGI application instance.
+        * returns: The application, wrapped or not.
         """
         # Wrap the callable, so we keep a reference to the app...
         app.wsgi_app = appstats_wsgi_middleware(app.wsgi_app)

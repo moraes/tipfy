856b3e77165a8fafc827abec65ca0e013d855d88
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-09-28 19:47:26 -0300

    New release.

--- experimental/setup.py
+++ experimental/setup.py
@@ -35,7 +35,7 @@ from setuptools import setup
 
 setup(
     name = 'tipfy-dev',
-    version = '0.6',
+    version = '0.7',
     license = 'BSD',
     url = 'http://www.tipfy.org/',
     download_url = 'http://www.tipfy.org/tipfy.tar.gz',

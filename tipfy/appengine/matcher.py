87d7c76f3b5030ca402ec5d3a0c7164e21f3ad30
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-11-27 09:36:09 -0200

    Unused imports.

--- tipfy/appengine/matcher.py
+++ tipfy/appengine/matcher.py
@@ -8,11 +8,9 @@
     :copyright: 2010 by tipfy.org.
     :license: Apache Software License, see LICENSE.txt for more details.
 """
-import logging
-
 from google.appengine.api import matcher
 
-from tipfy import RequestHandler, Response
+from tipfy import RequestHandler
 
 
 class MatcherHandler(RequestHandler):

e7c9bc6a9bdbff3420aca7c663d575aa531b2413
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-12-02 13:16:09 -0200

    Reverted jinja2.scripts until better define scripting tools.

--- tipfyext/jinja2/scripts.py
+++ tipfyext/jinja2/scripts.py
@@ -92,7 +92,23 @@ def compile_templates(argv=None):
     if argv is None:
         argv = sys.argv
 
-    set_gae_sys_path()
+    base_path = os.getcwd()
+    app_path = os.path.join(base_path, 'app')
+    gae_path = os.path.join(base_path, 'var/parts/google_appengine')
+
+    extra_paths = [
+        app_path,
+        gae_path,
+        # These paths are required by the SDK.
+        os.path.join(gae_path, 'lib', 'antlr3'),
+        os.path.join(gae_path, 'lib', 'django'),
+        os.path.join(gae_path, 'lib', 'ipaddr'),
+        os.path.join(gae_path, 'lib', 'webob'),
+        os.path.join(gae_path, 'lib', 'yaml', 'lib'),
+    ]
+
+    sys.path = extra_paths + sys.path
+
     from config import config
 
     app = Tipfy(config=config)

58be7f0cb25a6695e8fb2b63ecb9ac930490085b
Yesudeep Mangalapilly <gora.khargosh@gmail.com>
2011-03-20 16:41:43 +0530

    Adds a DecimalProperty property type for App Engine.
    Signed-off-by: Yesudeep Mangalapilly <gora.khargosh@gmail.com>

--- tipfy/appengine/db/properties.py
+++ tipfy/appengine/db/properties.py
@@ -10,6 +10,7 @@
 """
 import hashlib
 import pickle
+import decimal
 
 from google.appengine.ext import db
 
@@ -203,4 +204,26 @@ class TimezoneProperty(db.Property):
             return pytz.timezone(value)
 
         raise db.BadValueError("Property %s must be a pytz timezone or string."
+            % self.name)
+
+
+class DecimalProperty(db.Property):
+    """Stores a decimal value."""
+    data_type = decimal.Decimal
+
+    def get_value_for_datastore(self, model_instance):
+        return str(super(DecimalProperty, self).get_value_for_datastore(
+            model_instance))
+
+    def make_value_from_datastore(self, value):
+        return decimal.Decimal(value)
+
+    def validate(self, value):
+        value = super(DecimalProperty, self).validate(value)
+
+        if value is None or isinstance(value, decimal.Decimal):
+            return value
+        elif isinstance(value, basestring):
+            return decimal.Decimal(value)
+        raise db.BadValueError("Property %s must be a Decimal or string" 
             % self.name)

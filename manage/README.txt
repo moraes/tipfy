42b8d440f08b205c745a536fd8f2a6532d58b9ba
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-03-20 09:39:46 -0300

    Adding the (unfinished) manage scripts.

---
+++ manage/README.txt
@@ -0,0 +1,4 @@
+Management utilities
+====================
+This is unfinished and a work in progress. We need someone with the time to
+continue developing it.
---
+++ manage/utils.py
@@ -0,0 +1,31 @@
+def get_unique_sequence(seq):
+    seen = set()
+    return [x for x in seq if x not in seen and not seen.add(x)]
+
+
+def import_string(import_name, silent=False):
+    """Imports an object based on a string. If *silent* is True the return
+    value will be None if the import fails.
+
+    Simplified version of the function with same name from `Werkzeug`_. We
+    duplicate it here because this file should not depend on external packages.
+
+    :param import_name:
+        The dotted name for the object to import.
+    :param silent:
+        If True, import errors are ignored and None is returned instead.
+    :returns:
+        The imported object.
+    """
+    if isinstance(import_name, unicode):
+        return import_name.encode('utf-8')
+
+    try:
+        if '.' in import_name:
+            module, obj = import_name.rsplit('.', 1)
+            return getattr(__import__(module, None, None, [obj]), obj)
+        else:
+            return __import__(import_name)
+    except (ImportError, AttributeError):
+        if not silent:
+            raise

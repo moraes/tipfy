928f5ca6e0b02f27c2e4d559092609b0b55bf784
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-04-23 19:49:54 -0300

    lib -> lib_path
    --HG--
    branch : 1.0

--- project/app/set_sys_path.py
+++ project/app/set_sys_path.py
@@ -13,7 +13,7 @@ lib_path = os.path.join(current_path, 'lib')
 def set_path():
     # Add lib as primary libraries directory, with fallback to lib/dist
     # and optionally to lib/dist.zip, loaded using zipimport.
-    if lib not in sys.path:
+    if lib_path not in sys.path:
         sys.path[0:0] = [
             lib_path,
             os.path.join(lib_path, 'dist'),
